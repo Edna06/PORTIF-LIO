d3778daf36ef4dcddb811b9d36a14cf0195689d9
Edna Maria Farias Moreira <edna.maria887@gmail.com>
2022-12-29 21:23:37 -0300

    adicionando eventos

---
+++ main.js
@@ -0,0 +1,28 @@
+const backToTopButton = document.querySelector('a#backToTopButton')
+
+
+window.addEventListener('scroll', showBackToTopButtonOnScroll)
+
+
+
+
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 300) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '100px',
+  duration: 600
+}).reveal(`
+.title,
+.card,
+#knowledge ul li,
+#contact a,
+#contact input,
+#contact textarea`)
